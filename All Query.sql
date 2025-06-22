60a3ff74c36ced5d589f2b5c00b5608bc17294b0
Aniket Mishra <ishuaniket007@gmail.com>
2025-06-23 00:59:32 +0530

    Add files via upload

---
+++ All Query.sql
@@ -0,0 +1,87 @@
+
+-- 1. Retrieve the total number of orders placed.
+
+SELECT 
+    COUNT(order_id) AS total_number_of_orders_placed
+FROM
+    orders;
+
+--  2. Calculate the total revenue generated from pizza sales.
+
+SELECT 
+    ROUND(SUM(order_details.quantity * pizzas.price),
+            2) AS Generated_revenue
+FROM
+    order_details
+        JOIN
+    pizzas ON pizzas.pizza_id = order_details.pizza_id;
+
+
+-- 3. Identify the highest-priced pizza.
+
+SELECT 
+    pizza_types.name, pizzas.price
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+ORDER BY price DESC
+LIMIT 1;
+
+-- 4  Identify the most common pizza size ordered.
+
+SELECT 
+    pizzas.size,
+    COUNT(order_details.order_details_id) AS order_count
+FROM
+    pizzas
+        JOIN
+    order_details ON pizzas.pizza_id = order_details.pizza_id
+GROUP BY pizzas.size
+ORDER BY order_count DESC
+LIMIT 1;
+
+-- List the top 5 most ordered pizza types along with their quantities.
+
+SELECT 
+    pizza_types.name,
+    SUM(order_details.quantity) AS order_quantity
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    order_details ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.name
+ORDER BY order_quantity DESC
+LIMIT 5;
+    
+-- Intermediate:
+
+-- Join the necessary tables to find the total
+--  quantity of each pizza category ordered.
+
+SELECT pizza_types.category,SUM(order_details.quantity) AS quantity
+FROM pizza_types JOIN pizzas
+ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+JOIN order_details
+ON order_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.category ORDER BY quantity DESC;
+
+
+
+-- Determine the distribution of orders by hour of the day.
+
+-- Join relevant tables to find the category-wise distribution of pizzas.
+
+-- Group the orders by date and calculate the average number of pizzas ordered per day.
+
+-- Determine the top 3 most ordered pizza types based on revenue.
+
+-- Advanced:
+
+-- Calculate the percentage contribution of each pizza type to total revenue.
+
+-- Analyze the cumulative revenue generated over time.
+
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
